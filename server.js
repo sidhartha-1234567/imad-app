bfcaf670deb27136fe1f50aee59e5b08fa6ece5f
sidhartha-1234567 <30803306+sidhartha-1234567@users.noreply.github.com>
2017-08-29 10:27:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article-one',function(req,res){
+req.send('Article one is requested and will be served here');
+    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
