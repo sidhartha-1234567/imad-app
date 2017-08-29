536005a340c923b1abbddbe480bd4e1fea7dc467
sidhartha-1234567 <30803306+sidhartha-1234567@users.noreply.github.com>
2017-08-29 10:56:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ app.get('/', function (req, res) {
 
 
 app.get('/Article-one',function(req,res){
-req.send('Article one is requested and will be served here');
-    
+res.sendFile(path.join(__dirname, 'ui', 'article one.html'));
 });
 
 app.get('/Article-two',function(req,res){
