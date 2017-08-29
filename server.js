cc6998e3e7ed3e9b5c1157edc2da1a1bc263b304
sidhartha-1234567 <30803306+sidhartha-1234567@users.noreply.github.com>
2017-08-30 00:49:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,62 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleone = {
+    
+   title :'Article One I am Sidhartha Tripathy ',
+   heading :'Article one',
+   date :'29 october,2017',
+   content :`
+           '<p>
+           This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+       <p>
+       
+           This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+       '</p>
+           This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+       <p>
+           This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
+      </p>
+     
+     function CreateTemplete (data){ 
+     var title=data.title;
+     var date=data.date;
+     var heading=data.heading;
+     var content=dara.cotent;
+    
+    var html templete =
+     <html>
+<head>
+   <title>
+        ${title}
+   </title> 
+     
+     <link href="/ui/style.css" rel="stylesheet" />
+  </head> 
+<body>
+    <div> 
+<div>
+    <a href='/'>  Home</a>
+
+   <hr/>
+   <h3>
+      ${heading}
+   </h3>
+   <div>
+       ${date}
+   </div>
+  <div class="container">
+       ${content}
+   </div>
+   </div>
+</div>
+</body>
+</html>
+return html Templete;
+}
+    
+};
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
