77358e1759010dfda755d87618f71bdd661b600b
sidhartha-1234567 <30803306+sidhartha-1234567@users.noreply.github.com>
2017-08-31 10:49:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,9 +94,6 @@ app.get('/ui/madi.png', function (req, res) {
 
 var port = 80;
 app.listen(port, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-
+  console.log(`IMAD course app listening on port ${port}!`});
 });
-
-
 
