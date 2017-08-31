e96986949a4c70c174b50aa4c2831c86e38ee988
sidhartha-1234567 <30803306+sidhartha-1234567@users.noreply.github.com>
2017-08-31 10:38:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var articleone = {
    title :'Article One I am Sidhartha Tripathy ',
    heading :'Article one',
    date :'29 october,2017',
+
    content :`
            '<p>
            This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
@@ -22,13 +23,13 @@ var articleone = {
        <p>
            This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
       </p>
-     
+     };
      function CreateTemplete (data){ 
      var title=data.title;
      var date=data.date;
      var heading=data.heading;
      var content=dara.cotent;
-    
+     
     var html templete =
      <html>
 <head>
@@ -36,7 +37,7 @@ var articleone = {
         ${title}
    </title> 
      
-     <link href="/ui/style.css" rel="stylesheet" />
+     <link href="/ui/style.css" rel="stylesheet"/>
   </head> 
 <body>
     <div> 
@@ -57,17 +58,16 @@ var articleone = {
 </div>
 </body>
 </html>
+  :
 return html Templete;
 }
-    
-};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
 app.get('/Article-one',function(req,res){
-res.sendFile(path.join(__dirname, 'ui', 'article one.html'));
+res.send(CreateTemplete(Article one));
 });
 
 app.get('/Article-two',function(req,res){
@@ -95,4 +95,8 @@ app.get('/ui/madi.png', function (req, res) {
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
+
 });
+
+
+
